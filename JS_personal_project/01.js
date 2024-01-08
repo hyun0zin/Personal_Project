b0d67dcde0084444b2f1e625f1d2ee52772baa19
HyunjinKim <koujie11@gmail.com>
2024-01-08 12:13:50 +0900

    js 파일 업로드

---
+++ JS_personal_project/01.js
@@ -0,0 +1,17 @@
+function getMoive(){
+  const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYzFjNzNiNzQ4N2IwMTM5ZTI2MDU5MjFhZTBhMjA0MCIsInN1YiI6IjY1OTY1YTFmMzI2ZWMxNGU2ZDA2YzFkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h6L2OeVyMqWiyUQs3Qi5f-6rwiWL-k8Oku-0G_5f6S0'
+    }
+};
+
+fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+)
+    .then((response) => response.json())
+    .then((response) => {console.log(response)})
+    .catch((err) => console.error(err));
+}
